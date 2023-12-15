e0939b4904e08b180b0bf586ece1152ec3a5d7a4
DARK-SHANU <153761002+DARK-SHANU@users.noreply.github.com>
2023-12-15 15:54:22 +0530

    Update downloadSocialMedia.js

--- plugins/downloadSocialMedia.js
+++ plugins/downloadSocialMedia.js
@@ -53,54 +53,46 @@ AMDI({ cmd: ["ig", "insta", "instagram"], desc: Lang.igDesc, example: Lang.igEXA
     }
 }));
 
- AMDI({ cmd: 
-     
-const axios = require('axios')
-const cheerio = require('cheerio')
 
-async function CyberMDTiktok (Url) {
-	return new Promise (async (resolve, reject) => {
-		await axios.request({
-			url: "https://ttdownloader.com/",
-			method: "GET",
-			headers: {
-				"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-				"accept-language": "en-US,en;q=0.9,id;q=0.8",
-				"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
-				"cookie": "_ga=GA1.2.1240046717.1620835673; PHPSESSID=i14curq5t8omcljj1hlle52762; popCookie=1; _gid=GA1.2.1936694796.1623913934"
-			}
-		}).then(respon => {
-			const $ = cheerio.load(respon.data)
-			const token = $('#token').attr('value')
-			axios({
-				url: "https://ttdownloader.com/req/",
-				method: "POST",
-				data: new URLSearchParams(Object.entries({url: Url, format: "", token: token})),
-				headers: {
-					"accept": "*/*",
-					"accept-language": "en-US,en;q=0.9,id;q=0.8",
-					"content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-					"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
-					"cookie": "_ga=GA1.2.1240046717.1620835673; PHPSESSID=i14curq5t8omcljj1hlle52762; popCookie=1; _gid=GA1.2.1936694796.1623913934"
-				}
-			}).then(res => {
-				const ch = cheerio.load(res.data)
-				const result = {
-					status: res.status,
-					result: {
-						nowatermark: ch('#results-list > div:nth-child(2)').find('div.download > a').attr('href'),
-						watermark: ch('#results-list > div:nth-child(3)').find('div.download > a').attr('href'),
-						audio: ch('#results-list > div:nth-child(4)').find(' div.download > a').attr('href')
-					}
-				}
-				resolve(result)
-			}).catch(reject)
-		}).catch(reject)
-	})
-}
+ AMDI({ cmd: ["tk", "tiktok"], desc: Lang.TKDESC, example: Lang.tkEXA, type: "download", react: "🏳‍🌈" }, (async (amdiWA) => {
+    let { input, prefix, reply, sendListMsg } = amdiWA.msgLayout;
 
-module.exports = { CyberMDTiktok }
+    if (!input) return await reply(Lang.needlink, '❓');
+    if (!input.includes('tiktok.com/')) return await reply(Lang.needlink, '❓');
 
+    const tkData = await tiktok({ url: input });
+
+    const TKText = \\\${tkData.video.signature}\\\\n\n🎵 Music: ${tkData.audio.name}\n\n👨🏻‍🎤 Author: ${tkData.owner.name}\n\n👤 Username: ${tkData.owner.username}
+
+    const sections = [
+        {
+            title: "Tiktok Information",
+            rows: [
+                { title: "ℹ Tiktok Information", rowId: `${prefix}tkinfo ${input}` }
+            ]
+        },
+        {
+            title: "Tiktok Video",
+            rows: [
+                { title: "🔖 With Watermark", rowId: `${prefix}tkdl mark ${input}` },
+                { title: "📼 No-Watermark", rowId: `${prefix}tkdl nomark ${input}` }
+            ]
+        },
+        {
+            title: "Tiktok Audio",
+            rows: [
+                { title: "🎶 Audio File", rowId: `${prefix}tkdl audio ${input}` },
+                { title: "📁 Document File", rowId: `${prefix}tkdl doc ${input}` }
+            ]
+        }
+    ]
+
+    var listInfo = {}
+    listInfo.title = "🎞 Tiktok Downloader"
+    listInfo.text = TKText
+    listInfo.buttonTXT = "Download now"
+
+    return await sendListMsg(listInfo, sections);
 }));
 
 
