d6596e7324a888f7a86380d4262ee52801a935de
DARK-SHANU <153761002+DARK-SHANU@users.noreply.github.com>
2023-12-17 17:01:35 +0530

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -16,7 +16,7 @@ bot.addCommand(
     })
     const date = new Date()
     let CMD_HELP = `╭────────────────╮
-						ʟᴇᴠᴀɴᴛᴇʀ
+						KING QUEEN-MD
 ╰────────────────╯
 
 ╭────────────────
@@ -86,7 +86,7 @@ bot.addCommand(
       }
     })
     const date = new Date()
-    let msg = `\`\`\`╭═══ LEVANTER ═══⊷
+    let msg = `\`\`\`╭═══ KING QUEEN-MD ═══⊷
 ┃❃╭──────────────
 ┃❃│ Prefix : ${PREFIX}
 ┃❃│ User : ${message.pushName}
